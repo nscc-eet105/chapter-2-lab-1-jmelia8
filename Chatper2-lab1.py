f9a63a1b2e9a506a589ea868ded9ad5f1887403a
Joe Melia <jmelia@students.nortwheststate.edu>
2025-06-16 14:06:02 -0400

    Assignment Complete

---
+++ Chatper2-lab1.py
@@ -0,0 +1,9 @@
+#Joe Melia
+#EET-107
+#6/13/25
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+marbles = int(input("Enter the number of marbles you wish to purchase: "))
+
+print("Oder prepared for", fname, lname, "\n \n", marbles, "marbles ordered @ $1.20 \n \n Totl cost is $", float(marbles * 1.20))
